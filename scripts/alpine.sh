33cb22f542c331016ad1f5e3906b4aa1c7135aa9
maalos <fasterd91@gmail.com>
2022-05-17 20:17:19 +0200

    another bugfix #3

--- scripts/alpine.sh
+++ scripts/alpine.sh
@@ -54,7 +54,7 @@ if [ -n "\$(ls -A alpine-binds)"  ]; then
 fi
 command+=" -b /dev"
 command+=" -b /proc"
-command+=" -b $dir/root:/dev/shm"
+command+=" -b ${dir}/root:/dev/shm"
 ## uncomment the following line to have access to the home directory of termux
 #command+=" -b /data/data/com.termux/files/home:/root"
 ## uncomment the following line to mount /sdcard directly to /
